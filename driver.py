2ae4c7955d61d707bb08ea48c8c4575eb2c86a37
Alberto Rodriguez <betoca@gmail.com>
2023-09-29 04:43:12 -0600

    Matching file for inputs, single embedded output

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import sys
+import os
 from typing import List
 
 from pyspark.sql import SparkSession
@@ -13,9 +14,8 @@ def init():
 
     global SPARK
     SPARK = SparkSession.builder.appName("Echo Test").getOrCreate()
-
     conf = SPARK.sparkContext.getConf()
-    print("sparkTmpJobWorkingFolder = ", conf.get("sparkTmpJobWorkingFolder"))
+
     print("spark.startDate = ", conf.get("spark.startDate"))
     print(str(conf.getAll()))
     print("Sys Arguments" , sys.argv)
@@ -24,24 +24,25 @@ def init():
 # modelop.score
 def score(external_inputs: List, external_outputs: List, external_model_assets: List):
     startYear = SPARK.sparkContext.getConf().get("spark.startDate")
+    outputDir = SPARK.sparkContext.getConf().get("spark.outputDir")
 
+    output_asset_path = external_outputs[0]["fileUrl"]
     # Iterate through the inputs
     for idx in range(len(external_inputs)):
-        # Simply echo the input (with filtered year) if there's a matching output definition
-        if len(external_outputs) > idx:
-            input_asset = external_inputs[idx]
-            output_asset_path = external_outputs[idx]["fileUrl"]
-            input_asset_path = input_asset["fileUrl"]
+        # Simply echo the input (with filtered year)
+        input_asset = external_inputs[idx]
+        input_asset_path = input_asset["fileUrl"]
+        basename = os.path.basename(input_asset_path)
 
-            # Fail if assets are JSON
-            if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
-                raise ValueError("Input file format is set as JSON but must be CSV")
+        # Fail if assets are JSON
+        if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
+            raise ValueError("Input file format is set as JSON but must be CSV")
 
-            df = SPARK.read.format("csv").option("header", "true").load(input_asset_path)
-            df.filter(df.Year > startYear)
+        df = SPARK.read.format("csv").option("header", "true").load(input_asset_path)
+        df.filter(df.Year > startYear)
 
-            # Use coalesce() so that the output CSV is a single file for easy reading
-            input_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(output_asset_path)
+        # Use coalesce() so that the output CSV is a single file for easy reading
+        df.coalesce(1).write.mode("overwrite").option("header", "true").csv(outputDir + "/" + basename)
 
     SPARK.stop()
 
