a11710582433b668d07a343fb0aa4ca8958dc393
Alberto Rodriguez <betoca@gmail.com>
2023-09-29 03:58:01 -0600

    process multiple inputs

--- driver.py
+++ driver.py
@@ -23,14 +23,24 @@ def init():
 
 # modelop.score
 def score(external_inputs: List, external_outputs: List, external_model_assets: List):
-    # Grab single input asset and single output asset file paths
-    input_asset_path, output_asset_path = parse_assets(
-        external_inputs, external_outputs
-    )
-    input_df = SPARK.read.format("csv").option("header", "true").load(input_asset_path)
+    startYear = SPARK.sparkContext.getConf().get("spark.startDate")
 
-    # Use coalesce() so that the output CSV is a single file for easy reading
-    input_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(output_asset_path)
+    # Iterate through the inputs
+    for idx, input_asset in external_inputs:
+        # Simply echo the input (with filtered year) if there's a matching output definition
+        if len(external_outputs) > idx:
+            output_asset_path = external_outputs[idx]["fileUrl"]
+            input_asset_path = input_asset["fileUrl"]
+
+            # Fail if assets are JSON
+            if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
+                raise ValueError("Input file format is set as JSON but must be CSV")
+
+            df = SPARK.read.format("csv").option("header", "true").load(input_asset_path)
+            df.filter(df.Year > startYear)
+
+            # Use coalesce() so that the output CSV is a single file for easy reading
+            input_df.coalesce(1).write.mode("overwrite").option("header", "true").csv(output_asset_path)
 
     SPARK.stop()
 
@@ -38,22 +48,3 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
 # modelop.metrics
 def metrics(external_inputs: List, external_outputs: List, external_model_assets: List):
     score(external_inputs, external_outputs, external_model_assets)
-
-
-def parse_assets(external_inputs: List, external_outputs: List):
-    """Returns a tuple (input asset hdfs path, output asset hdfs path)"""
-
-    # There's only one key-value pair in each dict, so
-    # grab the first value from both
-    input_asset = external_inputs[0]
-    output_asset = external_outputs[0]
-
-    # Fail if assets are JSON
-    if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
-        raise ValueError("Input file format is set as JSON but must be CSV")
-
-    # Return paths from file URLs
-    input_asset_path = input_asset["fileUrl"]
-    output_asset_path = output_asset["fileUrl"]
-
-    return (input_asset_path, output_asset_path)
