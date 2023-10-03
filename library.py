4fd24d4b834317ee75bb0d58a8ec262652ed6394
Alberto Rodriguez <betoca@gmail.com>
2023-10-03 01:32:10 -0600

    Fix asset variable name

--- library.py
+++ library.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, isnull, when, count, udf, to_json, spark_partition_id, collect_list, struct
 
 
-def validate_input_format(asset):
+def validate_input_format(input_asset):
     if ("fileFormat" in input_asset) and (input_asset["fileFormat"] == "JSON"):
         raise ValueError("Input file format is set as JSON but must be CSV")
 
