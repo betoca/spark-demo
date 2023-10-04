c1b846cbd0e028fdeb8918a39b871804896fcae1
Alberto Rodriguez <betoca@gmail.com>
2023-10-03 22:31:45 -0600

    Test to write json dict

--- driver.py
+++ driver.py
@@ -8,7 +8,7 @@ import pandas as pd
 import json
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, isnull, when, count, udf, to_json, spark_partition_id, collect_list, struct
+from pyspark.sql.functions import col, isnull, when, count, udf, to_json, spark_partition_id, collect_list, struct, Row
 import library
 
 
@@ -46,17 +46,48 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
 
         df_list.update({basename + " data" : list(df.toPandas().to_dict('records'))})
 
-        if "Max" in df.columns:
-            bar_chart = {basename + " max chart" : {"title" : "Example Bar Chart","x_axis_label": "X Axis","y_axis_label": "Y Axis","rotated": False,"data" : {"year": list(df.select(df.Year).toPandas().to_dict('list')),"max": list(df.select(df.Max).toPandas().to_dict('list'))},"categories": ["cat1", "cat2", "cat3", "cat4"]}}
-            df_list.update(bar_chart)
+        # if "Max" in df.columns:
+        #     bar_chart = {
+        #         basename + " max chart" : {
+        #             "title" : "Example Bar Chart",
+        #             "x_axis_label": "X Axis",
+        #             "y_axis_label": "Y Axis",
+        #             "rotated": False,
+        #             "data" : {
+        #               "year": list(df.select(df.Year).toPandas().to_dict('list')["Year"]),
+        #               "max": list(df.select(df.Max).toPandas().to_dict('list')["Max"])
+        #             },
+        #             "categories": ["cat1", "cat2", "cat3", "cat4"]
+        #         }
+        #     }
+        #
+        #     df_list.update(bar_chart)
+        print(basename + " schema:")
+        df.printSchema()
 
         # Use coalesce() so that the output CSV is a single file for easy reading
         df.coalesce(1).write.mode("overwrite").option("header", "true").csv(str(outputDir) + "/" + str(basename))
 
     # merged_df = reduce(lambda x, y: x.join(y, how = 'outer'), df_list)
     # merged_df.coalesce(1).write.mode("overwrite").json(output_asset_path)
-    print(df_list)
-    SPARK.read.json(SPARK.sparkContext.parallelize([df_list])).coalesce(1).write.mode('overwrite').json(output_asset_path)
+
+    # print(df_list)
+    # generic = SPARK.read.json(SPARK.sparkContext.parallelize([df_list])).coalesce(1)
+    # print("Merged schema:")
+    # generic.printSchema()
+    # generic.write.mode('overwrite').json(output_asset_path)
+
+    SPARK.createDataFrame([df_list]).coalesce(1).write.mode('overwrite').json(output_asset_path)
+
+    # rdd_of_rows = rdd.map(lambda x: Row(**x))
+    # df = sql.createDataFrame(rdd_of_rows)
+    # df.printSchema()
+    # df = df_from_rdd(rdd, prototype, sqlContext)
+
+    # json_object = json.dumps(df_list)
+    # # Writing to sample.json
+    # with open("sample.json", "w") as outfile:
+    #     outfile.write(json_object)
 
     SPARK.stop()
 
