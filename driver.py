82a0211be4865f539c9c61b34e67b4387302bde4
Alberto Rodriguez <betoca@gmail.com>
2023-10-05 23:31:08 -0600

    Code cleanup

--- driver.py
+++ driver.py
@@ -3,15 +3,13 @@ from __future__ import print_function
 import sys
 import os
 from typing import List
-from functools import reduce
-import pandas as pd
-import json
 
-from pyspark.sql import SparkSession, Row
+from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import col, isnull, when, count, udf, to_json, spark_partition_id, collect_list, struct
-import library
-import mtr_format
+import transform_library as lib
+import mtr_format as mtr
+
+SPARK: SparkSession
 
 
 # modelop.init
@@ -24,7 +22,7 @@ def init():
 
     print("spark.startDate = ", conf.get("spark.startDate"))
     print(str(conf.getAll()))
-    print("Sys Arguments" , sys.argv)
+    print("Sys Arguments", sys.argv)
 
 
 # modelop.score
@@ -32,7 +30,7 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
     outputDir = SPARK.sparkContext.getConf().get("spark.outputDir")
     output_asset_path = external_outputs[0]["fileUrl"]
 
-    df_list = {}
+    mtr_output = {}
     schema_field_list = []
     # Iterate through the inputs
     for idx in range(len(external_inputs)):
@@ -42,37 +40,21 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
         basename = os.path.basename(input_asset_path)
 
         # Fail if assets are JSON
-        library.validate_input_format(input_asset)
+        lib.validate_input_format(input_asset)
 
-        df = library.load(SPARK, input_asset_path)
-        df = library.transform(SPARK, df)
+        df = lib.load(SPARK, input_asset_path)
+        df = lib.transform(SPARK, df)
 
-        df_list.update({basename : list(df.toPandas().to_dict('records'))})
-        schema_field_list.append(mtr_format.generic_table_schema_field(basename))
+        mtr_output.update(mtr.as_tabular_data(df))
+        schema_field_list.append(mtr.generic_table_schema_field(basename))
 
         if "Max" in df.columns:
-            bar_chart = {
-                basename + "_bar_graph" : {
-                    "title" : "Example Bar Chart",
-                    "x_axis_label": "X Axis",
-                    "y_axis_label": "Y Axis",
-                    "rotated": False,
-                    "data" : {
+            mtr_output.update(mtr.as_bar_chart_data({
                       "max": list(df.select(df.Max).toPandas().to_dict('list')["Max"]),
                       "min": list(df.select(df.Min).toPandas().to_dict('list')["Min"]),
-                    },
-                    "categories": list(df.select(df.Year).toPandas().to_dict('list')["Year"])
-                }
-            }
-
-            df_list.update(bar_chart)
-            schema_field_list.append(
-                mtr_format.bar_graph_schema_field(
-                    bar_chart_title = basename + "_bar_graph",
-                    bar_chart_data = ['max', 'min'],
-                    categories_type = StringType()
-               )
-            )
+                    }, categories=list(df.select(df.Year).toPandas().to_dict('list')["Year"]),
+                key=basename + " bar chart", title=basename, x_axis_label="Year", y_axis_label="Values", rotated=True))
+            schema_field_list.append(mtr.bar_graph_schema_field(bar_chart_title=basename + "_bar_graph", bar_chart_col_names=['max', 'min'], categories_type=IntegerType()))
 
         print(basename + " schema:")
         df.printSchema()
@@ -80,31 +62,12 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
         # Use coalesce() so that the output CSV is a single file for easy reading
         df.coalesce(1).write.mode("overwrite").option("header", "true").csv(str(outputDir) + "/" + str(basename))
 
-    # merged_df = reduce(lambda x, y: x.join(y, how = 'outer'), df_list)
-    # merged_df.coalesce(1).write.mode("overwrite").json(output_asset_path)
-
-    print(df_list)
-    # generic = SPARK.read.json(SPARK.sparkContext.parallelize([df_list])).coalesce(1)
-    # print("Merged schema:")
-    # generic.printSchema()
-    # generic.write.mode('overwrite').json(output_asset_path)
+    print(mtr_output)
 
     schema = StructType(schema_field_list)
-    row = Row(**df_list)
+    row = Row(**mtr_output)
     df = SPARK.createDataFrame([row], schema)
     df.coalesce(1).write.mode('overwrite').json(output_asset_path)
-
-    # SPARK.createDataFrame([df_list]).coalesce(1).write.mode('overwrite').json(output_asset_path)
-
-    # rdd_of_rows = rdd.map(lambda x: Row(**x))
-    # df = sql.createDataFrame(rdd_of_rows)
-    # df.printSchema()
-    # df = df_from_rdd(rdd, prototype, sqlContext)
-
-    # json_object = json.dumps(df_list)
-    # # Writing to sample.json
-    # with open("sample.json", "w") as outfile:
-    #     outfile.write(json_object)
 
     SPARK.stop()
 
--- mtr_format.py
+++ mtr_format.py
@@ -1,7 +1,7 @@
-from pyspark.sql import Row
 from pyspark.sql.types import *
 
-def bar_graph_schema_field(bar_chart_col_names = None, bar_chart_title = None, bar_chart_data = [] , categories_type = IntegerType()):
+
+def bar_graph_schema_field(bar_chart_title=None, bar_chart_col_names=None, categories_type=StringType()):
     partial_schema = StructField(
         bar_chart_title,
         StructType([
@@ -14,12 +14,48 @@ def bar_graph_schema_field(bar_chart_col_names = None, bar_chart_title = None, b
                 StructField("min", ArrayType(StringType()))
             ]), False),
             StructField("categories", ArrayType(categories_type), False),
-        ]),
-            True
+        ]), True
     )
     return partial_schema
 
 
-def generic_table_schema_field(title = "generic_table"):
-    partial_schema = StructField(title,ArrayType(MapType(StringType(),StringType())))
+def generic_table_schema_field(title="generic_table"):
+    partial_schema = StructField(title, ArrayType(MapType(StringType(), StringType())))
     return partial_schema
+
+
+def as_tabular_data(dataframe, key=None):
+    """
+    Returns a Dictionary with the data in records format
+    :param dataframe: A Spark DataFrame to return in tabular form
+    :param key: The string to use as key to the dictionary entry
+    :return: The Dictionary structure with the data
+    """
+    if key is not None:
+        return {key: list(dataframe.toPandas().to_dict('records'))}
+    else:
+        return list(dataframe.toPandas().to_dict('records'))
+
+
+def as_bar_chart_data(data, categories, key, title="", x_axis_label="", y_axis_label="", rotated=False):
+    """
+    Returns a dictionary with the structure necessary for a bar chart
+    :param data: a dictionary in the format: {"series1": ["a","b","c"], "series2": ["x","y","z"]}
+    :param categories: a list of values matching the series in the data
+    :param key: of the returning dictionary entry
+    :param title: of the bar chart
+    :param x_axis_label: text to describe the values in the categories
+    :param y_axis_label: text to describe the values in the data
+    :param rotated: true for horizontal, false for vertical (default)
+    :return: The dictionary structure with bar chart data
+    """
+    return {
+                key: {
+                    "title": title,
+                    "x_axis_label": x_axis_label,
+                    "y_axis_label": y_axis_label,
+                    "rotated": rotated,
+                    "data": data,
+                    "categories": categories
+                }
+            }
