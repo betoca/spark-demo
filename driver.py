1e8616c0e0d2a8498752a170d126b0e00a6946c1
Alberto Rodriguez <betoca@gmail.com>
2023-10-06 00:46:22 -0600

    Add count by year

--- driver.py
+++ driver.py
@@ -54,6 +54,13 @@ def score(external_inputs: List, external_outputs: List, external_model_assets: 
                 key=basename + "_bar_graph", title=basename, x_axis_label="Year", y_axis_label="Values", rotated=True))
             schema_field_list.append(mtr.bar_graph_schema_field(bar_chart_title=basename + "_bar_graph", bar_chart_col_names=['max', 'min']))
 
+        if "Count" in df.columns:
+            mtr_output.update(mtr.as_bar_chart_data({
+                      "count": list(df.select(df.Max).toPandas().to_dict('list')["Count"]),
+                    }, categories=list(df.select(df.Year).toPandas().to_dict('list')["Year"]),
+                key=basename + "_count_x_year", title=basename, x_axis_label="Year", y_axis_label="Values", rotated=False))
+            schema_field_list.append(mtr.bar_graph_schema_field(bar_chart_title=basename + "_count_x_year", bar_chart_col_names=['count']))
+
         print(basename + " schema:")
         df.printSchema()
 
